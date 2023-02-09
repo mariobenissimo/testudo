83c46a4e72567a0326bfc1c7dceebe2eea2f789c
Mara Mihali <mihalimara22@gmail.com>
2023-02-09 16:59:52 +0000

    update bench to blstr

--- benches/r1cs.rs
+++ benches/r1cs.rs
@@ -1,10 +1,11 @@
-use std::time::Instant;
-
+use libspartan::parameters::poseidon_params_bls12381;
+use libspartan::parameters::PoseidonConfiguration;
 use libspartan::{
   parameters::POSEIDON_PARAMETERS_FR_377, poseidon_transcript::PoseidonTranscript, Instance,
   NIZKGens, NIZK,
 };
 use serde::Serialize;
+use std::time::Instant;
 
 #[derive(Default, Clone, Serialize)]
 struct BenchmarkResults {
@@ -28,26 +29,38 @@ fn main() {
     br.power = s;
     br.input_constraints = num_cons;
     let num_inputs = 10;
-
     let start = Instant::now();
-    let (inst, vars, inputs) = Instance::produce_synthetic_r1cs(num_cons, num_vars, num_inputs);
+    let (inst, vars, inputs) =
+      Instance::<ark_blst::Scalar>::produce_synthetic_r1cs(num_cons, num_vars, num_inputs);
     let _duration = start.elapsed().as_millis();
-    let mut prover_transcript = PoseidonTranscript::new(&POSEIDON_PARAMETERS_FR_377);
+    let mut prover_transcript = PoseidonTranscript::new(&ark_blst::Scalar::poseidon_params());
 
-    let gens = NIZKGens::new(num_cons, num_vars, num_inputs);
+    let gens = NIZKGens::<ark_blst::Bls12>::new(num_cons, num_vars, num_inputs);
 
     let start = Instant::now();
-    let proof = NIZK::prove(&inst, vars, &inputs, &gens, &mut prover_transcript);
+    let proof = NIZK::<ark_blst::Bls12>::prove(&inst, vars, &inputs, &gens, &mut prover_transcript);
     let duration = start.elapsed().as_millis();
     br.spartan_proving_time = duration;
 
-    let mut verifier_transcript = PoseidonTranscript::new(&POSEIDON_PARAMETERS_FR_377);
-    let res = proof.verify(&inst, &inputs, &mut verifier_transcript, &gens);
+    let mut verifier_transcript = PoseidonTranscript::new(&ark_blst::Scalar::poseidon_params());
+    let res = proof.verify(
+      &inst,
+      &inputs,
+      &mut verifier_transcript,
+      &gens,
+      ark_blst::Scalar::poseidon_params(),
+    );
     assert!(res.is_ok());
     br.spartan_verifier_circuit_constraints = res.unwrap();
 
-    let mut verifier_transcript = PoseidonTranscript::new(&POSEIDON_PARAMETERS_FR_377);
-    let res = proof.verify_groth16(&inst, &inputs, &mut verifier_transcript, &gens);
+    let mut verifier_transcript = PoseidonTranscript::new(&ark_blst::Scalar::poseidon_params());
+    let res = proof.verify_groth16(
+      &inst,
+      &inputs,
+      &mut verifier_transcript,
+      &gens,
+      ark_blst::Scalar::poseidon_params(),
+    );
     assert!(res.is_ok());
 
     let (ds, dp, dv) = res.unwrap();
